857f0a28829a848cfb04b6133593d2d450687f1f
Diarmaid <@MadcapOcelot>
2016-10-09 17:03:20 +0100

    moving java install into playbook. moving to community roles.

--- reset.sh
+++ reset.sh
@@ -1,5 +1,6 @@
-vagrant destroy logging loadbalancer staticweb foi
+vagrant destroy logging staticweb foi scheduledtasks
 ssh-keygen -f "/home/e/.ssh/known_hosts" -R 10.10.1.249
+ssh-keygen -f "/home/e/.ssh/known_hosts" -R 10.10.1.248
 ssh-keygen -f "/home/e/.ssh/known_hosts" -R 10.10.1.10
 ssh-keygen -f "/home/e/.ssh/known_hosts" -R 10.10.1.11
 ssh-keygen -f "/home/e/.ssh/known_hosts" -R 10.10.1.12
